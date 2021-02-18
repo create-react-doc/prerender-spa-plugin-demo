b4984f0ba41215b8073802c46758eed67a5f56d5
mujue <mujue@pinduoduo.com>
2021-02-18 08:48:54 +0800

    feat: update

---
+++ build/precache-manifest.d92a48daa56206eeed606ab8d2aaf42d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cba66b7631739d7b549",
+    "url": "/static/css/main.6595c79d.chunk.css"
+  },
+  {
+    "revision": "5cba66b7631739d7b549",
+    "url": "/static/js/main.5cba66b7.chunk.js"
+  },
+  {
+    "revision": "f7da04801a020c52ad54",
+    "url": "/static/js/1.f7da0480.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8197ea0dbd4017e1880c463e1ffb3d2a",
+    "url": "/index.html"
+  }
+];
